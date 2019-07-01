d94d7c5dd77fac19e0592d5eedd5182f3c229fd6
David Newhall II <david@sleepers.pro>
2019-06-30 17:55:26 -0700

    Add custom data unmarshaller for durations.

--- starr.go
+++ starr.go
@@ -14,19 +14,28 @@ import (
 
 // Config is the data needed to poll Radarr or Sonarr.
 type Config struct {
-	APIKey   string        `json:"api_key" toml:"api_key" xml:"api_key" yaml:"api_key"`
-	URL      string        `json:"url" toml:"url" xml:"url" yaml:"url"`
-	Tag      string        `json:"tag" toml:"tag" xml:"tag" yaml:"tag"` // not used
-	HTTPPass string        `json:"http_pass" toml:"http_pass" xml:"http_pass" yaml:"http_pass"`
-	HTTPUser string        `json:"http_user" toml:"http_user" xml:"http_user" yaml:"http_user"`
-	Timeout  time.Duration `json:"timeout" toml:"timeout" xml:"timeout" yaml:"timeout"`
+	APIKey   string   `json:"api_key" toml:"api_key" xml:"api_key" yaml:"api_key"`
+	URL      string   `json:"url" toml:"url" xml:"url" yaml:"url"`
+	Tag      string   `json:"tag" toml:"tag" xml:"tag" yaml:"tag"` // not used
+	HTTPPass string   `json:"http_pass" toml:"http_pass" xml:"http_pass" yaml:"http_pass"`
+	HTTPUser string   `json:"http_user" toml:"http_user" xml:"http_user" yaml:"http_user"`
+	Timeout  Duration `json:"timeout" toml:"timeout" xml:"timeout" yaml:"timeout"`
+}
+
+// Duration is used to UnmarshalTOML into a time.Duration value.
+type Duration struct{ time.Duration }
+
+// UnmarshalText parses a duration type from a config file.
+func (d *Duration) UnmarshalText(data []byte) (err error) {
+	d.Duration, err = time.ParseDuration(string(data))
+	return
 }
 
 // Req makes a http request, with some additions.
 // path = "/query", params = "sort_by=timeleft&order=asc" (as url.Values)
 func (c *Config) Req(path string, params url.Values) ([]byte, error) {
 	client := &http.Client{
-		Timeout:   c.Timeout,
+		Timeout:   c.Timeout.Duration,
 		Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
 	}
 	path = c.fixPath(path)
