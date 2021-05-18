db987e46e26947f675a2a05fb1f93d8cb2eaba56
David Newhall II <david@sleepers.pro>
2021-05-17 23:30:35 -0700

    add methods for lidarr and readarr too

--- lidarr/lidarr.go
+++ lidarr/lidarr.go
@@ -33,6 +33,38 @@ func (l *Lidarr) GetQualityProfiles() ([]*QualityProfile, error) {
 	return profiles, nil
 }
 
+// AddQualityProfile updates a quality profile in place.
+func (l *Lidarr) AddQualityProfile(profile *QualityProfile) (int64, error) {
+	post, err := json.Marshal(profile)
+	if err != nil {
+		return 0, fmt.Errorf("json.Marshal(profile): %w", err)
+	}
+
+	var output QualityProfile
+
+	err = l.PostInto("v1/qualityProfile", nil, post, &output)
+	if err != nil {
+		return 0, fmt.Errorf("api.Post(qualityProfile): %w", err)
+	}
+
+	return output.ID, nil
+}
+
+// UpdateQualityProfile updates a quality profile in place.
+func (l *Lidarr) UpdateQualityProfile(profile *QualityProfile) error {
+	put, err := json.Marshal(profile)
+	if err != nil {
+		return fmt.Errorf("json.Marshal(profile): %w", err)
+	}
+
+	_, err = l.Put("v1/qualityProfile/"+strconv.FormatInt(profile.ID, 10), nil, put)
+	if err != nil {
+		return fmt.Errorf("api.Put(qualityProfile): %w", err)
+	}
+
+	return nil
+}
+
 // GetRootFolders returns all configured root folders.
 func (l *Lidarr) GetRootFolders() ([]*RootFolder, error) {
 	var folders []*RootFolder
