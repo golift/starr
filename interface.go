95b0dc053a637ebc69256e482f4c62784a209eed
David Newhall II <david@sleepers.pro>
2021-08-21 14:49:24 -0700

    use status text

--- interface.go
+++ interface.go
@@ -28,8 +28,8 @@ var _ APIer = (*Config)(nil)
 // Get makes a GET http request and returns the body.
 func (c *Config) Get(path string, params url.Values) ([]byte, error) {
 	code, data, err := c.req(path, http.MethodGet, params, nil)
-	c.Debugf("Sent (%s) to %s, Response (%d): %s (err: %v)",
-		http.MethodGet, c.setPathParams(path, params), code, string(data), err)
+	c.Debugf("Sent (%s) to %s, Response (%s): %s (err: %v)",
+		http.MethodGet, c.setPathParams(path, params), http.StatusText(code), string(data), err)
 
 	return data, err
 }
@@ -37,8 +37,8 @@ func (c *Config) Get(path string, params url.Values) ([]byte, error) {
 // Get makes a DELETE http request and returns the body.
 func (c *Config) Delete(path string, params url.Values) ([]byte, error) {
 	code, data, err := c.req(path, http.MethodDelete, params, nil)
-	c.Debugf("Sent (%s) to %s, Response (%d): %s (err: %v)",
-		http.MethodDelete, c.setPathParams(path, params), code, string(data), err)
+	c.Debugf("Sent (%s) to %s, Response (%s): %s (err: %v)",
+		http.MethodDelete, c.setPathParams(path, params), http.StatusText(code), string(data), err)
 
 	return data, err
 }
@@ -46,8 +46,8 @@ func (c *Config) Delete(path string, params url.Values) ([]byte, error) {
 // Put makes a PUT http request and returns the body.
 func (c *Config) Put(path string, params url.Values, body []byte) ([]byte, error) {
 	code, data, err := c.req(path, http.MethodPut, params, bytes.NewBuffer(body))
-	c.Debugf("Sent (%s) %d bytes to %s: %s\n Response (%d): %s (err: %v)",
-		http.MethodPut, len(body), c.setPathParams(path, params), string(body), code, string(data), err)
+	c.Debugf("Sent (%s) %d bytes to %s: %s\n Response (%s): %s (err: %v)",
+		http.MethodPut, len(body), c.setPathParams(path, params), string(body), http.StatusText(code), string(data), err)
 
 	return data, err
 }
@@ -55,8 +55,8 @@ func (c *Config) Put(path string, params url.Values, body []byte) ([]byte, error
 // Post makes a POST http request and returns the body.
 func (c *Config) Post(path string, params url.Values, body []byte) ([]byte, error) {
 	code, data, err := c.req(path, http.MethodPost, params, bytes.NewBuffer(body))
-	c.Debugf("Sent (%s) %d bytes to %s: %s\n Response (%d): %s (err: %v)",
-		http.MethodPost, len(body), c.setPathParams(path, params), string(body), code, string(data), err)
+	c.Debugf("Sent (%s) %d bytes to %s: %s\n Response (%s): %s (err: %v)",
+		http.MethodPost, len(body), c.setPathParams(path, params), string(body), http.StatusText(code), string(data), err)
 
 	return data, err
 }
