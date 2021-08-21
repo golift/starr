2a622f86675b6943a94107563486c7dbff022cb1
David Newhall II <david@sleepers.pro>
2021-08-21 13:56:24 -0700

    capture response code for log too

--- http.go
+++ http.go
@@ -15,9 +15,9 @@ import (
 
 /* The methods in this file provided assumption-ridden HTTP calls for Starr apps. */
 
-func (c *Config) req(path, method string, params url.Values, body io.Reader) ([]byte, error) {
+func (c *Config) req(path, method string, params url.Values, body io.Reader) (int, []byte, error) {
 	if c.Client == nil { // we must have an http client.
-		return nil, ErrNilClient
+		return 0, nil, ErrNilClient
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), c.Timeout.Duration)
@@ -25,7 +25,7 @@ func (c *Config) req(path, method string, params url.Values, body io.Reader) ([]
 
 	req, err := http.NewRequestWithContext(ctx, method, c.setPathParams(path, params), body)
 	if err != nil {
-		return nil, fmt.Errorf("http.NewRequestWithContext(path): %w", err)
+		return 0, nil, fmt.Errorf("http.NewRequestWithContext(path): %w", err)
 	}
 
 	c.setHeaders(req)
@@ -51,25 +51,25 @@ func (c *Config) setHeaders(req *http.Request) {
 }
 
 // getBody makes an http request and returns the response body if there are no errors.
-func (c *Config) getBody(req *http.Request) ([]byte, error) {
+func (c *Config) getBody(req *http.Request) (int, []byte, error) {
 	resp, err := c.Client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("httpClient.Do(req): %w", err)
+		return 0, nil, fmt.Errorf("httpClient.Do(req): %w", err)
 	}
 	defer resp.Body.Close()
 
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("ioutil.ReadAll: %w", err)
+		return 0, nil, fmt.Errorf("ioutil.ReadAll: %w", err)
 	}
 
 	// fmt.Println(string(b))
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return b, fmt.Errorf("failed: %v (status: %s): %w: %s",
+		return resp.StatusCode, b, fmt.Errorf("failed: %v (status: %s): %w: %s",
 			resp.Request.RequestURI, resp.Status, ErrInvalidStatusCode, string(b))
 	}
 
-	return b, nil
+	return resp.StatusCode, b, nil
 }
 
 // setPathParams makes sure the path starts with /api and returns the full URL.
