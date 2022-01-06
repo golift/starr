fc64f840a3644db75651c28bfde6463db5fe4ce3
David Newhall II <david@sleepers.pro>
2022-01-05 23:54:55 -0800

    add context methods to prowlarr

--- prowlarr/system.go
+++ prowlarr/system.go
@@ -1,6 +1,7 @@
 package prowlarr
 
 import (
+	"context"
 	"fmt"
 
 	"golift.io/starr"
@@ -8,9 +9,14 @@ import (
 
 // GetSystemStatus returns system status.
 func (p *Prowlarr) GetSystemStatus() (*SystemStatus, error) {
+	return p.GetSystemStatusContext(context.Background())
+}
+
+// GetSystemStatusContext returns system status.
+func (p *Prowlarr) GetSystemStatusContext(ctx context.Context) (*SystemStatus, error) {
 	var status SystemStatus
 
-	err := p.GetInto("v1/system/status", nil, &status)
+	err := p.GetInto(ctx, "v1/system/status", nil, &status)
 	if err != nil {
 		return nil, fmt.Errorf("api.Get(system/status): %w", err)
 	}
@@ -21,9 +27,15 @@ func (p *Prowlarr) GetSystemStatus() (*SystemStatus, error) {
 // GetBackupFiles returns all available Prowlarr backup files.
 // Use GetBody to download a file using BackupFile.Path.
 func (p *Prowlarr) GetBackupFiles() ([]*starr.BackupFile, error) {
+	return p.GetBackupFilesContext(context.Background())
+}
+
+// GetBackupFiles returns all available Prowlarr backup files.
+// Use GetBody to download a file using BackupFile.Path.
+func (p *Prowlarr) GetBackupFilesContext(ctx context.Context) ([]*starr.BackupFile, error) {
 	var output []*starr.BackupFile
 
-	if err := p.GetInto("v1/system/backup", nil, &output); err != nil {
+	if err := p.GetInto(ctx, "v1/system/backup", nil, &output); err != nil {
 		return nil, fmt.Errorf("api.Get(system/backup): %w", err)
 	}
 
