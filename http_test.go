ea87cde23eb120c3a50a76ec522fe9d9dc6de628
David Newhall II <david@sleepers.pro>
2022-03-12 01:49:28 -0800

    add a few more tests for setpath

--- http_test.go
+++ http_test.go
@@ -15,12 +15,22 @@ func TestSetPath(t *testing.T) {
 	cnfg := starr.Config{URL: "http://short.zz"}
 
 	// These must all return the same value...
-	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("v1/test"))
-	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("v1/test/"))
-	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("/v1/test"))
-	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("/v1/test/"))
-	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("api/v1/test"))
+	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("v1/test"))     // no slashes.
+	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("v1/test/"))    // trailing slash.
+	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("/v1/test"))    // leading slash.
+	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("/v1/test/"))   // both slashes.
+	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("api/v1/test")) // ...and repeat.
 	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("api/v1/test/"))
 	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("/api/v1/test"))
 	assert.Equal(t, cnfg.URL+api+"/v1/test", cnfg.SetPath("/api/v1/test/"))
+
+	// These must all return the same value...
+	assert.Equal(t, cnfg.URL+api+"/v1/test/another/level", cnfg.SetPath("v1/test/another/level"))
+	assert.Equal(t, cnfg.URL+api+"/v1/test/another/level", cnfg.SetPath("v1/test/another/level/"))
+	assert.Equal(t, cnfg.URL+api+"/v1/test/another/level", cnfg.SetPath("/v1/test/another/level"))
+	assert.Equal(t, cnfg.URL+api+"/v1/test/another/level", cnfg.SetPath("/v1/test/another/level/"))
+	assert.Equal(t, cnfg.URL+api+"/v1/test/another/level", cnfg.SetPath("api/v1/test/another/level"))
+	assert.Equal(t, cnfg.URL+api+"/v1/test/another/level", cnfg.SetPath("api/v1/test/another/level/"))
+	assert.Equal(t, cnfg.URL+api+"/v1/test/another/level", cnfg.SetPath("/api/v1/test/another/level"))
+	assert.Equal(t, cnfg.URL+api+"/v1/test/another/level", cnfg.SetPath("/api/v1/test/another/level/"))
 }
