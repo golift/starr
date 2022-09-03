492ee3212209cb936bf9802189bb815386095bf0
David Newhall II <david@sleepers.pro>
2022-09-03 13:22:29 -0700

    use bas quality for quality defs

--- sonarr/qualitydefinition.go
+++ sonarr/qualitydefinition.go
@@ -13,13 +13,13 @@ import (
 
 // QualityDefinition is the /api/v3/qualitydefinition endpoint.
 type QualityDefinition struct {
-	ID       int64          `json:"id,omitempty"`
-	Weight   int64          `json:"weight"` // This should not be changed.
-	MinSize  float64        `json:"minSize"`
-	MaxSize  float64        `json:"maxSize"`
-	PrefSize float64        `json:"preferredSize"` // v4 only.
-	Title    string         `json:"title"`
-	Quality  *starr.Quality `json:"quality"`
+	ID       int64              `json:"id,omitempty"`
+	Weight   int64              `json:"weight"` // This should not be changed.
+	MinSize  float64            `json:"minSize"`
+	MaxSize  float64            `json:"maxSize"`
+	PrefSize float64            `json:"preferredSize"` // v4 only.
+	Title    string             `json:"title"`
+	Quality  *starr.BaseQuality `json:"quality"`
 }
 
 // Define Base Path for Quality Definition calls.
