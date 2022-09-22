c6ef1fc46bb28cfdf2d89e181f46c4c388df1dd2
Fuochi <ffuochi@hotmail.com>
2022-09-22 11:09:03 +0400

    feat(radarr): add rootfolder missing methods

--- radarr/rootfolder.go
+++ radarr/rootfolder.go
@@ -1,29 +1,33 @@
 package radarr
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"path"
 
 	"golift.io/starr"
 )
 
+// Define Base Path for rootFolder calls.
 const bpRootFolder = APIver + "/rootFolder"
 
-// RootFolder is the /rootFolder endpoint.
+// RootFolder is the /api/v3/rootfolder endpoint.
 type RootFolder struct {
-	ID              int64         `json:"id"`
+	Accessible      bool          `json:"accessible,omitempty"`
+	ID              int64         `json:"id,omitempty"`
+	FreeSpace       int64         `json:"freeSpace,omitempty"`
 	Path            string        `json:"path"`
-	Accessible      bool          `json:"accessible"`
-	FreeSpace       int64         `json:"freeSpace"`
-	UnmappedFolders []*starr.Path `json:"unmappedFolders"`
+	UnmappedFolders []*starr.Path `json:"unmappedFolders,omitempty"`
 }
 
-// GetRootFolders returns all configured root folders.
+// GetRootFolders returns all configured rootFolders.
 func (r *Radarr) GetRootFolders() ([]*RootFolder, error) {
 	return r.GetRootFoldersContext(context.Background())
 }
 
-// GetRootFoldersContext returns all configured root folders.
+// GetRootFoldersContext returns all configured rootFolders.
 func (r *Radarr) GetRootFoldersContext(ctx context.Context) ([]*RootFolder, error) {
 	var output []*RootFolder
 
@@ -33,4 +37,58 @@ func (r *Radarr) GetRootFoldersContext(ctx context.Context) ([]*RootFolder, erro
 	}
 
 	return output, nil
+}
+
+// GetRootFolder returns a single rootFolder.
+func (r *Radarr) GetRootFolder(folderID int) (*RootFolder, error) {
+	return r.GetRootFolderContext(context.Background(), folderID)
+}
+
+// GetRootFolderContext returns a single rootFolder.
+func (r *Radarr) GetRootFolderContext(ctx context.Context, folderID int) (*RootFolder, error) {
+	var output RootFolder
+
+	req := starr.Request{URI: path.Join(bpRootFolder, fmt.Sprint(folderID))}
+	if err := r.GetInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Get(%s): %w", &req, err)
+	}
+
+	return &output, nil
+}
+
+// AddRootFolder creates a rootFolder.
+func (r *Radarr) AddRootFolder(folder *RootFolder) (*RootFolder, error) {
+	return r.AddRootFolderContext(context.Background(), folder)
+}
+
+// AddRootFolderContext creates a rootFolder.
+func (r *Radarr) AddRootFolderContext(ctx context.Context, folder *RootFolder) (*RootFolder, error) {
+	var output RootFolder
+
+	var body bytes.Buffer
+	if err := json.NewEncoder(&body).Encode(folder); err != nil {
+		return nil, fmt.Errorf("json.Marshal(%s): %w", bpRootFolder, err)
+	}
+
+	req := starr.Request{URI: bpRootFolder, Body: &body}
+	if err := r.PostInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Post(%s): %w", &req, err)
+	}
+
+	return &output, nil
+}
+
+// DeleteRootFolder removes a single rootFolder.
+func (r *Radarr) DeleteRootFolder(folderID int) error {
+	return r.DeleteRootFolderContext(context.Background(), folderID)
+}
+
+// DeleteRootFolderContext removes a single rootFolder.
+func (r *Radarr) DeleteRootFolderContext(ctx context.Context, folderID int) error {
+	req := starr.Request{URI: path.Join(bpRootFolder, fmt.Sprint(folderID))}
+	if err := r.DeleteAny(ctx, req); err != nil {
+		return fmt.Errorf("api.Delete(%s): %w", &req, err)
+	}
+
+	return nil
 }
