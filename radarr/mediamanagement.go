47df201ab4c2657cdb6e99293613ffc497b0c1c5
Fuochi <ffuochi@hotmail.com>
2022-09-22 11:01:28 +0400

    feat(radarr): add mediamanagement

---
+++ radarr/mediamanagement.go
@@ -0,0 +1,76 @@
+package radarr
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"golift.io/starr"
+)
+
+// Define Base Path for mediaManagement calls.
+const bpMediaManagement = APIver + "/config/mediaManagement"
+
+// MediaManagement represents the /config/mediaManagement endpoint.
+type MediaManagement struct {
+	AutoRenameFolders                       bool   `json:"autoRenameFolders,omitempty"`
+	AutoUnmonitorPreviouslyDownloadedMovies bool   `json:"autoUnmonitorPreviouslyDownloadedMovies,omitempty"`
+	CopyUsingHardlinks                      bool   `json:"copyUsingHardlinks,omitempty"`
+	CreateEmptyMovieFolders                 bool   `json:"createEmptyMovieFolders,omitempty"`
+	DeleteEmptyFolders                      bool   `json:"deleteEmptyFolders,omitempty"`
+	EnableMediaInfo                         bool   `json:"enableMediaInfo,omitempty"`
+	ImportExtraFiles                        bool   `json:"importExtraFiles,omitempty"`
+	PathsDefaultStatic                      bool   `json:"pathsDefaultStatic,omitempty"`
+	SetPermissionsLinux                     bool   `json:"setPermissionsLinux,omitempty"`
+	SkipFreeSpaceCheckWhenImporting         bool   `json:"skipFreeSpaceCheckWhenImporting,omitempty"`
+	ID                                      int64  `json:"id,omitempty"`
+	MinimumFreeSpaceWhenImporting           int64  `json:"minimumFreeSpaceWhenImporting,omitempty"`
+	RecycleBinCleanupDays                   int64  `json:"recycleBinCleanupDays,omitempty"`
+	ChmodFolder                             string `json:"chmodFolder,omitempty"`
+	ChownGroup                              string `json:"chownGroup,omitempty"`
+	DownloadPropersAndRepacks               string `json:"downloadPropersAndRepacks,omitempty"`
+	ExtraFileExtensions                     string `json:"extraFileExtensions,omitempty"`
+	FileDate                                string `json:"fileDate,omitempty"`
+	RecycleBin                              string `json:"recycleBin,omitempty"`
+	RescanAfterRefresh                      string `json:"rescanAfterRefresh,omitempty"`
+}
+
+// GetMediaManagement returns the mediaManagement.
+func (r *Radarr) GetMediaManagement() (*MediaManagement, error) {
+	return r.GetMediaManagementContext(context.Background())
+}
+
+// GetMediaManagement returns the mediaManagement.
+func (r *Radarr) GetMediaManagementContext(ctx context.Context) (*MediaManagement, error) {
+	var output MediaManagement
+
+	req := starr.Request{URI: bpMediaManagement}
+	if err := r.GetInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Get(%s): %w", &req, err)
+	}
+
+	return &output, nil
+}
+
+// UpdateMediaManagement updates the mediaManagement.
+func (r *Radarr) UpdateMediaManagement(mMgt *MediaManagement) (*MediaManagement, error) {
+	return r.UpdateMediaManagementContext(context.Background(), mMgt)
+}
+
+// UpdateMediaManagementContext updates the mediaManagement.
+func (r *Radarr) UpdateMediaManagementContext(ctx context.Context, mMgt *MediaManagement) (*MediaManagement, error) {
+	var output MediaManagement
+
+	var body bytes.Buffer
+	if err := json.NewEncoder(&body).Encode(mMgt); err != nil {
+		return nil, fmt.Errorf("json.Marshal(%s): %w", bpMediaManagement, err)
+	}
+
+	req := starr.Request{URI: bpMediaManagement, Body: &body}
+	if err := r.PutInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Put(%s): %w", &req, err)
+	}
+
+	return &output, nil
+}
---
+++ radarr/mediamanagement_test.go
@@ -0,0 +1,157 @@
+package radarr_test
+
+import (
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golift.io/starr"
+	"golift.io/starr/radarr"
+)
+
+const mediaManagementBody = `{
+  "autoUnmonitorPreviouslyDownloadedMovies": false,
+  "recycleBin": "",
+  "recycleBinCleanupDays": 7,
+  "downloadPropersAndRepacks": "preferAndUpgrade",
+  "createEmptyMovieFolders": false,
+  "deleteEmptyFolders": false,
+  "fileDate": "none",
+  "rescanAfterRefresh": "always",
+  "autoRenameFolders": false,
+  "pathsDefaultStatic": false,
+  "setPermissionsLinux": false,
+  "chmodFolder": "755",
+  "chownGroup": "",
+  "skipFreeSpaceCheckWhenImporting": false,
+  "minimumFreeSpaceWhenImporting": 100,
+  "copyUsingHardlinks": true,
+  "importExtraFiles": false,
+  "extraFileExtensions": "srt",
+  "enableMediaInfo": true,
+  "id": 1
+}`
+
+func TestGetMediaManagement(t *testing.T) {
+	t.Parallel()
+
+	tests := []*starr.TestMockData{
+		{
+			Name:           "200",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "config/mediaManagement"),
+			ExpectedMethod: "GET",
+			ResponseStatus: 200,
+			ResponseBody:   mediaManagementBody,
+			WithResponse: &radarr.MediaManagement{
+				AutoRenameFolders:                       false,
+				AutoUnmonitorPreviouslyDownloadedMovies: false,
+				CopyUsingHardlinks:                      true,
+				CreateEmptyMovieFolders:                 false,
+				DeleteEmptyFolders:                      false,
+				EnableMediaInfo:                         true,
+				ImportExtraFiles:                        false,
+				PathsDefaultStatic:                      false,
+				SetPermissionsLinux:                     false,
+				SkipFreeSpaceCheckWhenImporting:         false,
+				ID:                                      1,
+				MinimumFreeSpaceWhenImporting:           100,
+				RecycleBinCleanupDays:                   7,
+				ChmodFolder:                             "755",
+				ChownGroup:                              "",
+				DownloadPropersAndRepacks:               "preferAndUpgrade",
+				ExtraFileExtensions:                     "srt",
+				FileDate:                                "none",
+				RecycleBin:                              "",
+				RescanAfterRefresh:                      "always",
+			},
+			WithError: nil,
+		},
+		{
+			Name:           "404",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "config/mediaManagement"),
+			ExpectedMethod: "GET",
+			ResponseStatus: 404,
+			ResponseBody:   `{"message": "NotFound"}`,
+			WithError:      starr.ErrInvalidStatusCode,
+			WithResponse:   (*radarr.MediaManagement)(nil),
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+			mockServer := test.GetMockServer(t)
+			client := radarr.New(starr.New("mockAPIkey", mockServer.URL, 0))
+			output, err := client.GetMediaManagement()
+			assert.ErrorIs(t, err, test.WithError, "error is not the same as expected")
+			assert.EqualValues(t, test.WithResponse, output, "response is not the same as expected")
+		})
+	}
+}
+
+func TestUpdateMediaManagement(t *testing.T) {
+	t.Parallel()
+
+	tests := []*starr.TestMockData{
+		{
+			Name:           "202",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "config/mediaManagement"),
+			ExpectedMethod: "PUT",
+			ResponseStatus: 202,
+			WithRequest: &radarr.MediaManagement{
+				EnableMediaInfo: true,
+			},
+			ExpectedRequest: `{"enableMediaInfo":true}` + "\n",
+			ResponseBody:    mediaManagementBody,
+			WithResponse: &radarr.MediaManagement{
+				AutoRenameFolders:                       false,
+				AutoUnmonitorPreviouslyDownloadedMovies: false,
+				CopyUsingHardlinks:                      true,
+				CreateEmptyMovieFolders:                 false,
+				DeleteEmptyFolders:                      false,
+				EnableMediaInfo:                         true,
+				ImportExtraFiles:                        false,
+				PathsDefaultStatic:                      false,
+				SetPermissionsLinux:                     false,
+				SkipFreeSpaceCheckWhenImporting:         false,
+				ID:                                      1,
+				MinimumFreeSpaceWhenImporting:           100,
+				RecycleBinCleanupDays:                   7,
+				ChmodFolder:                             "755",
+				ChownGroup:                              "",
+				DownloadPropersAndRepacks:               "preferAndUpgrade",
+				ExtraFileExtensions:                     "srt",
+				FileDate:                                "none",
+				RecycleBin:                              "",
+				RescanAfterRefresh:                      "always",
+			},
+			WithError: nil,
+		},
+		{
+			Name:           "404",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "config/mediaManagement"),
+			ExpectedMethod: "PUT",
+			WithRequest: &radarr.MediaManagement{
+				EnableMediaInfo: true,
+			},
+			ExpectedRequest: `{"enableMediaInfo":true}` + "\n",
+			ResponseStatus:  404,
+			ResponseBody:    `{"message": "NotFound"}`,
+			WithError:       starr.ErrInvalidStatusCode,
+			WithResponse:    (*radarr.MediaManagement)(nil),
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+			mockServer := test.GetMockServer(t)
+			client := radarr.New(starr.New("mockAPIkey", mockServer.URL, 0))
+			output, err := client.UpdateMediaManagement(test.WithRequest.(*radarr.MediaManagement))
+			assert.ErrorIs(t, err, test.WithError, "error is not the same as expected")
+			assert.EqualValues(t, test.WithResponse, output, "response is not the same as expected")
+		})
+	}
+}
