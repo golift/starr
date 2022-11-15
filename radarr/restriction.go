e7470b45b8d07ca7419a1671e7c4e1db5ef49e77
Fuochi <ffuochi@hotmail.com>
2022-11-15 07:42:25 +0400

    feat(radarr): indexer (#80)
    * feat(radarr): add indexer
    
    * feat(radarr): add indexer config
    
    * feat(radarr): add indexer restriction

---
+++ radarr/restriction.go
@@ -0,0 +1,114 @@
+package radarr
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"path"
+
+	"golift.io/starr"
+)
+
+const bpRestriction = APIver + "/restriction"
+
+// Restriction is the input for a new or updated restriction.
+type Restriction struct {
+	Tags     []int  `json:"tags,omitempty"`
+	Required string `json:"required,omitempty"`
+	Ignored  string `json:"ignored,omitempty"`
+	ID       int64  `json:"id,omitempty"`
+}
+
+// GetRestrictions returns all configured restrictions.
+func (r *Radarr) GetRestrictions() ([]*Restriction, error) {
+	return r.GetRestrictionsContext(context.Background())
+}
+
+// GetRestrictionsContext returns all configured restrictions.
+func (r *Radarr) GetRestrictionsContext(ctx context.Context) ([]*Restriction, error) {
+	var output []*Restriction
+
+	req := starr.Request{URI: bpRestriction}
+	if err := r.GetInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Get(%s): %w", &req, err)
+	}
+
+	return output, nil
+}
+
+// GetRestriction returns a single restriction.
+func (r *Radarr) GetRestriction(restrictionID int64) (*Restriction, error) {
+	return r.GetRestrictionContext(context.Background(), restrictionID)
+}
+
+// GetIndGetRestrictionContextexer returns a single restriction.
+func (r *Radarr) GetRestrictionContext(ctx context.Context, restrictionID int64) (*Restriction, error) {
+	var output Restriction
+
+	req := starr.Request{URI: path.Join(bpRestriction, fmt.Sprint(restrictionID))}
+	if err := r.GetInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Get(%s): %w", &req, err)
+	}
+
+	return &output, nil
+}
+
+// AddRestriction creates a restriction.
+func (r *Radarr) AddRestriction(restriction *Restriction) (*Restriction, error) {
+	return r.AddRestrictionContext(context.Background(), restriction)
+}
+
+// AddRestrictionContext creates a restriction.
+func (r *Radarr) AddRestrictionContext(ctx context.Context, restriction *Restriction) (*Restriction, error) {
+	var output Restriction
+
+	var body bytes.Buffer
+	if err := json.NewEncoder(&body).Encode(restriction); err != nil {
+		return nil, fmt.Errorf("json.Marshal(%s): %w", bpRestriction, err)
+	}
+
+	req := starr.Request{URI: bpRestriction, Body: &body}
+	if err := r.PostInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Post(%s): %w", &req, err)
+	}
+
+	return &output, nil
+}
+
+// UpdateRestriction updates the restriction.
+func (r *Radarr) UpdateRestriction(restriction *Restriction) (*Restriction, error) {
+	return r.UpdateRestrictionContext(context.Background(), restriction)
+}
+
+// UpdateRestrictionContext updates the restriction.
+func (r *Radarr) UpdateRestrictionContext(ctx context.Context, restriction *Restriction) (*Restriction, error) {
+	var output Restriction
+
+	var body bytes.Buffer
+	if err := json.NewEncoder(&body).Encode(restriction); err != nil {
+		return nil, fmt.Errorf("json.Marshal(%s): %w", bpRestriction, err)
+	}
+
+	req := starr.Request{URI: path.Join(bpRestriction, fmt.Sprint(restriction.ID)), Body: &body}
+	if err := r.PutInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Put(%s): %w", &req, err)
+	}
+
+	return &output, nil
+}
+
+// DeleteRestriction removes a single restriction.
+func (r *Radarr) DeleteRestriction(restrictionID int64) error {
+	return r.DeleteRestrictionContext(context.Background(), restrictionID)
+}
+
+// DeleteRestrictionContext removes a single restriction.
+func (r *Radarr) DeleteRestrictionContext(ctx context.Context, restrictionID int64) error {
+	req := starr.Request{URI: path.Join(bpRestriction, fmt.Sprint(restrictionID))}
+	if err := r.DeleteAny(ctx, req); err != nil {
+		return fmt.Errorf("api.Delete(%s): %w", &req, err)
+	}
+
+	return nil
+}
---
+++ radarr/restriction_test.go
@@ -0,0 +1,247 @@
+package radarr_test
+
+import (
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golift.io/starr"
+	"golift.io/starr/radarr"
+)
+
+const restrictionBody = `{
+	"required": "test1",
+	"ignored": "test2",
+	"tags": [],
+	"id": 2
+  }`
+
+func TestGetRestrictions(t *testing.T) {
+	t.Parallel()
+
+	tests := []*starr.TestMockData{
+		{
+			Name:           "200",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "restriction"),
+			ExpectedMethod: "GET",
+			ResponseStatus: 200,
+			ResponseBody:   "[" + restrictionBody + "]",
+			WithResponse: []*radarr.Restriction{
+				{
+					Tags:     []int{},
+					Required: "test1",
+					Ignored:  "test2",
+					ID:       2,
+				},
+			},
+			WithError: nil,
+		},
+		{
+			Name:           "404",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "restriction"),
+			ExpectedMethod: "GET",
+			ResponseStatus: 404,
+			ResponseBody:   `{"message": "NotFound"}`,
+			WithError:      starr.ErrInvalidStatusCode,
+			WithResponse:   []*radarr.Restriction(nil),
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+			mockServer := test.GetMockServer(t)
+			client := radarr.New(starr.New("mockAPIkey", mockServer.URL, 0))
+			output, err := client.GetRestrictions()
+			assert.ErrorIs(t, err, test.WithError, "error is not the same as expected")
+			assert.EqualValues(t, test.WithResponse, output, "response is not the same as expected")
+		})
+	}
+}
+
+func TestGetRestriction(t *testing.T) {
+	t.Parallel()
+
+	tests := []*starr.TestMockData{
+		{
+			Name:           "200",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "restriction", "2"),
+			ExpectedMethod: "GET",
+			ResponseStatus: 200,
+			WithRequest:    int64(2),
+			ResponseBody:   restrictionBody,
+			WithResponse: &radarr.Restriction{
+				Tags:     []int{},
+				Required: "test1",
+				Ignored:  "test2",
+				ID:       2,
+			},
+			WithError: nil,
+		},
+		{
+			Name:           "404",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "restriction", "2"),
+			ExpectedMethod: "GET",
+			ResponseStatus: 404,
+			WithRequest:    int64(2),
+			ResponseBody:   `{"message": "NotFound"}`,
+			WithResponse:   (*radarr.Restriction)(nil),
+			WithError:      starr.ErrInvalidStatusCode,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+			mockServer := test.GetMockServer(t)
+			client := radarr.New(starr.New("mockAPIkey", mockServer.URL, 0))
+			output, err := client.GetRestriction(test.WithRequest.(int64))
+			assert.ErrorIs(t, err, test.WithError, "error is not the same as expected")
+			assert.EqualValues(t, test.WithResponse, output, "response is not the same as expected")
+		})
+	}
+}
+
+func TestAddRestriction(t *testing.T) {
+	t.Parallel()
+
+	tests := []*starr.TestMockData{
+		{
+			Name:           "200",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "restriction"),
+			ExpectedMethod: "POST",
+			ResponseStatus: 200,
+			WithRequest: &radarr.Restriction{
+				Required: "test1",
+				Ignored:  "test2",
+			},
+			ExpectedRequest: `{"required":"test1","ignored":"test2"}` + "\n",
+			ResponseBody:    restrictionBody,
+			WithResponse: &radarr.Restriction{
+				Tags:     []int{},
+				Required: "test1",
+				Ignored:  "test2",
+				ID:       2,
+			},
+			WithError: nil,
+		},
+		{
+			Name:           "200",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "restriction"),
+			ExpectedMethod: "POST",
+			ResponseStatus: 404,
+			WithRequest: &radarr.Restriction{
+				Required: "test1",
+				Ignored:  "test2",
+			},
+			ExpectedRequest: `{"required":"test1","ignored":"test2"}` + "\n",
+			ResponseBody:    `{"message": "NotFound"}`,
+			WithError:       starr.ErrInvalidStatusCode,
+			WithResponse:    (*radarr.Restriction)(nil),
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+			mockServer := test.GetMockServer(t)
+			client := radarr.New(starr.New("mockAPIkey", mockServer.URL, 0))
+			output, err := client.AddRestriction(test.WithRequest.(*radarr.Restriction))
+			assert.ErrorIs(t, err, test.WithError, "error is not the same as expected")
+			assert.EqualValues(t, test.WithResponse, output, "response is not the same as expected")
+		})
+	}
+}
+
+func TestUpdateRestriction(t *testing.T) {
+	t.Parallel()
+
+	tests := []*starr.TestMockData{
+		{
+			Name:           "200",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "restriction", "2"),
+			ExpectedMethod: "PUT",
+			ResponseStatus: 200,
+			WithRequest: &radarr.Restriction{
+				Required: "test1",
+				Ignored:  "test2",
+				ID:       2,
+			},
+			ExpectedRequest: `{"required":"test1","ignored":"test2","id":2}` + "\n",
+			ResponseBody:    restrictionBody,
+			WithResponse: &radarr.Restriction{
+				Tags:     []int{},
+				Required: "test1",
+				Ignored:  "test2",
+				ID:       2,
+			},
+			WithError: nil,
+		},
+		{
+			Name:           "404",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "restriction", "2"),
+			ExpectedMethod: "PUT",
+			ResponseStatus: 404,
+			WithRequest: &radarr.Restriction{
+				Required: "test1",
+				Ignored:  "test2",
+				ID:       2,
+			},
+			ExpectedRequest: `{"required":"test1","ignored":"test2","id":2}` + "\n",
+			ResponseBody:    `{"message": "NotFound"}`,
+			WithError:       starr.ErrInvalidStatusCode,
+			WithResponse:    (*radarr.Restriction)(nil),
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+			mockServer := test.GetMockServer(t)
+			client := radarr.New(starr.New("mockAPIkey", mockServer.URL, 0))
+			output, err := client.UpdateRestriction(test.WithRequest.(*radarr.Restriction))
+			assert.ErrorIs(t, err, test.WithError, "error is not the same as expected")
+			assert.EqualValues(t, test.WithResponse, output, "response is not the same as expected")
+		})
+	}
+}
+
+func TestDeleteRestriction(t *testing.T) {
+	t.Parallel()
+
+	tests := []*starr.TestMockData{
+		{
+			Name:           "200",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "restriction", "1"),
+			ExpectedMethod: "DELETE",
+			WithRequest:    int64(1),
+			ResponseStatus: 200,
+			ResponseBody:   "{}",
+			WithError:      nil,
+		},
+		{
+			Name:           "404",
+			ExpectedPath:   path.Join("/", starr.API, radarr.APIver, "restriction", "1"),
+			ExpectedMethod: "DELETE",
+			WithRequest:    int64(1),
+			ResponseStatus: 404,
+			ResponseBody:   `{"message": "NotFound"}`,
+			WithError:      starr.ErrInvalidStatusCode,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+			mockServer := test.GetMockServer(t)
+			client := radarr.New(starr.New("mockAPIkey", mockServer.URL, 0))
+			err := client.DeleteRestriction(test.WithRequest.(int64))
+			assert.ErrorIs(t, err, test.WithError, "error is not the same as expected")
+		})
+	}
+}
