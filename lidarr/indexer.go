8cc2ee1dc126caa35f472d0c5c864cfa6bb18f55
Fuochi <ffuochi@hotmail.com>
2022-11-23 14:06:06 +0400

    feat(lidarr): add indexer

---
+++ lidarr/indexer.go
@@ -0,0 +1,140 @@
+package lidarr
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"path"
+
+	"golift.io/starr"
+)
+
+const bpIndexer = APIver + "/indexer"
+
+// IndexerInput is the input for a new or updated indexer.
+type IndexerInput struct {
+	EnableAutomaticSearch   bool                `json:"enableAutomaticSearch"`
+	EnableInteractiveSearch bool                `json:"enableInteractiveSearch"`
+	EnableRss               bool                `json:"enableRss"`
+	Priority                int64               `json:"priority"`
+	ID                      int64               `json:"id,omitempty"`
+	ConfigContract          string              `json:"configContract"`
+	Implementation          string              `json:"implementation"`
+	Name                    string              `json:"name"`
+	Protocol                string              `json:"protocol"`
+	Tags                    []int               `json:"tags"`
+	Fields                  []*starr.FieldInput `json:"fields"`
+}
+
+// IndexerOutput is the output from the indexer methods.
+type IndexerOutput struct {
+	EnableAutomaticSearch   bool                 `json:"enableAutomaticSearch"`
+	EnableInteractiveSearch bool                 `json:"enableInteractiveSearch"`
+	EnableRss               bool                 `json:"enableRss"`
+	SupportsRss             bool                 `json:"supportsRss"`
+	SupportsSearch          bool                 `json:"supportsSearch"`
+	Priority                int64                `json:"priority"`
+	ID                      int64                `json:"id,omitempty"`
+	ConfigContract          string               `json:"configContract"`
+	Implementation          string               `json:"implementation"`
+	ImplementationName      string               `json:"implementationName"`
+	InfoLink                string               `json:"infoLink"`
+	Name                    string               `json:"name"`
+	Protocol                string               `json:"protocol"`
+	Tags                    []int                `json:"tags"`
+	Fields                  []*starr.FieldOutput `json:"fields"`
+}
+
+// GetIndexers returns all configured indexers.
+func (l *Lidarr) GetIndexers() ([]*IndexerOutput, error) {
+	return l.GetIndexersContext(context.Background())
+}
+
+// GetIndexersContext returns all configured indexers.
+func (l *Lidarr) GetIndexersContext(ctx context.Context) ([]*IndexerOutput, error) {
+	var output []*IndexerOutput
+
+	req := starr.Request{URI: bpIndexer}
+	if err := l.GetInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Get(%s): %w", &req, err)
+	}
+
+	return output, nil
+}
+
+// GetIndexer returns a single indexer.
+func (l *Lidarr) GetIndexer(indexerID int64) (*IndexerOutput, error) {
+	return l.GetIndexerContext(context.Background(), indexerID)
+}
+
+// GetIndGetIndexerContextexer returns a single indexer.
+func (l *Lidarr) GetIndexerContext(ctx context.Context, indexerID int64) (*IndexerOutput, error) {
+	var output IndexerOutput
+
+	req := starr.Request{URI: path.Join(bpIndexer, fmt.Sprint(indexerID))}
+	if err := l.GetInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Get(%s): %w", &req, err)
+	}
+
+	return &output, nil
+}
+
+// AddIndexer creates a indexer.
+func (l *Lidarr) AddIndexer(indexer *IndexerInput) (*IndexerOutput, error) {
+	return l.AddIndexerContext(context.Background(), indexer)
+}
+
+// AddIndexerContext creates a indexer.
+func (l *Lidarr) AddIndexerContext(ctx context.Context, indexer *IndexerInput) (*IndexerOutput, error) {
+	var output IndexerOutput
+
+	var body bytes.Buffer
+	if err := json.NewEncoder(&body).Encode(indexer); err != nil {
+		return nil, fmt.Errorf("json.Marshal(%s): %w", bpIndexer, err)
+	}
+
+	req := starr.Request{URI: bpIndexer, Body: &body}
+	if err := l.PostInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Post(%s): %w", &req, err)
+	}
+
+	return &output, nil
+}
+
+// UpdateIndexer updates the indexer.
+func (l *Lidarr) UpdateIndexer(indexer *IndexerInput) (*IndexerOutput, error) {
+	return l.UpdateIndexerContext(context.Background(), indexer)
+}
+
+// UpdateIndexerContext updates the indexer.
+func (l *Lidarr) UpdateIndexerContext(ctx context.Context, indexer *IndexerInput) (*IndexerOutput, error) {
+	var output IndexerOutput
+
+	var body bytes.Buffer
+	if err := json.NewEncoder(&body).Encode(indexer); err != nil {
+		return nil, fmt.Errorf("json.Marshal(%s): %w", bpIndexer, err)
+	}
+
+	req := starr.Request{URI: path.Join(bpIndexer, fmt.Sprint(indexer.ID)), Body: &body}
+	if err := l.PutInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Put(%s): %w", &req, err)
+	}
+
+	return &output, nil
+}
+
+// DeleteIndexer removes a single indexer.
+func (l *Lidarr) DeleteIndexer(indexerID int64) error {
+	return l.DeleteIndexerContext(context.Background(), indexerID)
+}
+
+// DeleteIndexerContext removes a single indexer.
+func (l *Lidarr) DeleteIndexerContext(ctx context.Context, indexerID int64) error {
+	req := starr.Request{URI: path.Join(bpIndexer, fmt.Sprint(indexerID))}
+	if err := l.DeleteAny(ctx, req); err != nil {
+		return fmt.Errorf("api.Delete(%s): %w", &req, err)
+	}
+
+	return nil
+}
