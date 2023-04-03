dbec60f4d3aedacc5d8bb139b7ba9b185c995b83
David Newhall II <david@sleepers.pro>
2023-04-03 00:46:19 -0700

    add block list for all four apps

---
+++ readarr/blocklist.go
@@ -0,0 +1,133 @@
+package readarr
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"path"
+	"time"
+
+	"golift.io/starr"
+)
+
+// Define Base Path for Block List queries.
+const bpBlocklist = APIver + "/blocklist"
+
+// BlockList represents the /api/v1/blocklist endpoint.
+type BlockList struct {
+	Page          int                `json:"page"`
+	PageSize      int                `json:"pageSize"`
+	SortKey       string             `json:"sortKey"`
+	SortDirection string             `json:"sortDirection"`
+	TotalRecords  int                `json:"totalRecords"`
+	Records       []*BlockListRecord `json:"records"`
+}
+
+// BlockListRecord represents a single block list item.
+type BlockListRecord struct {
+	Author        *Author        `json:"author"`
+	Quality       *starr.Quality `json:"quality"`
+	CustomFormats []interface{}  `json:"customFormats"`
+	BookIDs       []int64        `json:"bookIds"`
+	ID            int64          `json:"id"`
+	AuthorID      int64          `json:"authorId"`
+	Date          time.Time      `json:"date"`
+	SourceTitle   string         `json:"sourceTitle"`
+	Protocol      string         `json:"protocol"`
+	Indexer       string         `json:"indexer"`
+	Message       string         `json:"message"`
+}
+
+// GetBlockList returns the count of block list items requested.
+// If you need control over the page, use GetBlockListPage().
+func (r *Readarr) GetBlockList(count int) (*BlockList, error) {
+	return r.GetBlockListContext(context.Background(), count)
+}
+
+// GetBlockListContext returns block list items.
+func (r *Readarr) GetBlockListContext(ctx context.Context, records int) (*BlockList, error) {
+	list := &BlockList{Records: []*BlockListRecord{}}
+	perPage := starr.SetPerPage(records, 500)
+
+	for page := 1; ; page++ {
+		curr, err := r.GetBlockListPageContext(ctx, &starr.PageReq{PageSize: perPage, Page: page})
+		if err != nil {
+			return nil, err
+		}
+
+		list.Records = append(list.Records, curr.Records...)
+		if len(list.Records) >= curr.TotalRecords ||
+			(len(list.Records) >= records && records != 0) ||
+			len(curr.Records) == 0 {
+			list.PageSize = curr.TotalRecords
+			list.TotalRecords = curr.TotalRecords
+			list.SortDirection = curr.SortDirection
+			list.SortKey = curr.SortKey
+
+			break
+		}
+
+		perPage = starr.AdjustPerPage(records, curr.TotalRecords, len(list.Records), perPage)
+	}
+
+	return list, nil
+}
+
+// GetBlockListPage returns block list items based on filters.
+func (r *Readarr) GetBlockListPage(params *starr.PageReq) (*BlockList, error) {
+	return r.GetBlockListPageContext(context.Background(), params)
+}
+
+// GetBlockListPageContext returns block list items based on filters.
+func (r *Readarr) GetBlockListPageContext(ctx context.Context, params *starr.PageReq) (*BlockList, error) {
+	var output BlockList
+
+	params.CheckSet("sortKey", "date")
+
+	req := starr.Request{URI: bpBlocklist, Query: params.Params()}
+	if err := r.GetInto(ctx, req, &output); err != nil {
+		return nil, fmt.Errorf("api.Get(%s): %w", &req, err)
+	}
+
+	return &output, nil
+}
+
+// DeleteBlockList removes a single block list item.
+func (r *Readarr) DeleteBlockList(listID int64) error {
+	return r.DeleteBlockListContext(context.Background(), listID)
+}
+
+// DeleteBlockListContext removes a single block list item with a context.
+func (r *Readarr) DeleteBlockListContext(ctx context.Context, listID int64) error {
+	req := starr.Request{URI: path.Join(bpBlocklist, fmt.Sprint(listID))}
+	if err := r.DeleteAny(ctx, req); err != nil {
+		return fmt.Errorf("api.Delete(%s): %w", &req, err)
+	}
+
+	return nil
+}
+
+// DeleteBlockLists removes multiple block list items.
+func (r *Readarr) DeleteBlockLists(ids []int64) error {
+	return r.DeleteBlockListsContext(context.Background(), ids)
+}
+
+// DeleteBlockListsContext removes multiple block list items with a context.
+func (r *Readarr) DeleteBlockListsContext(ctx context.Context, ids []int64) error {
+	input := struct {
+		IDs []int64 `json:"ids"`
+	}{IDs: ids}
+
+	var body bytes.Buffer
+	if err := json.NewEncoder(&body).Encode(input); err != nil {
+		return fmt.Errorf("json.Marshal(%s): %w", bpBlocklist, err)
+	}
+
+	req := starr.Request{URI: path.Join(bpBlocklist, "bulk"), Body: &body}
+	if err := r.DeleteAny(ctx, req); err != nil {
+		return fmt.Errorf("api.Delete(%s): %w", &req, err)
+	}
+
+	return nil
+}
