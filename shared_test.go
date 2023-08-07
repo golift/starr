ca9a79546577335be995119c4ecc7a1a6e0f1875
David Newhall II <david@sleepers.pro>
2023-08-07 00:46:33 -0700

    add test

---
+++ shared_test.go
@@ -0,0 +1,30 @@
+package starr_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golift.io/starr"
+)
+
+func TestQueueDeleteOpts_Values(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+
+	var opts *starr.QueueDeleteOpts
+
+	params := opts.Values() // it's nil.
+	assert.Equal(params.Encode(), "removeFromClient=true",
+		"default queue delete parameters encoded incorrectly")
+
+	opts = &starr.QueueDeleteOpts{
+		BlockList:        true,
+		RemoveFromClient: starr.False(),
+		SkipRedownload:   true,
+	}
+	params = opts.Values()
+
+	assert.Equal("false", params.Get("removeFromClient"), "delete parameters encoded incorrectly")
+	assert.Equal("true", params.Get("blocklist"), "delete parameters encoded incorrectly")
+	assert.Equal("true", params.Get("skipRedownload"), "delete parameters encoded incorrectly")
+}
