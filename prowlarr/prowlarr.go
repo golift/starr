3a6a0994553b420ee3d34eee9a1a9fa6af35e194
David Newhall <david@sleepers.pro>
2024-03-01 13:10:39 -0800

    Add ping endpoints (#142)
    * Add ping endpoints
    
    * fix ping path

--- prowlarr/prowlarr.go
+++ prowlarr/prowlarr.go
@@ -1,6 +1,8 @@
 package prowlarr
 
 import (
+	"context"
+	"fmt"
 	"strings"
 
 	"golift.io/starr"
@@ -23,4 +25,25 @@ func New(config *starr.Config) *Prowlarr {
 	config.URL = strings.TrimSuffix(config.URL, "/")
 
 	return &Prowlarr{APIer: config}
+}
+
+// bp means base path. You'll see it a lot in these files.
+const bpPing = "/ping" // ping has no api or version prefix.
+
+// Ping returns an error if the Prowlarr instance does not respond with a 200 to an HTTP /ping request.
+func (p *Prowlarr) Ping() error {
+	return p.PingContext(context.Background())
+}
+
+// PingContext returns an error if the Prowlarr instance does not respond with a 200 to an HTTP /ping request.
+func (p *Prowlarr) PingContext(ctx context.Context) error {
+	req := starr.Request{URI: bpPing}
+
+	resp, err := p.Get(ctx, req)
+	if err != nil {
+		return fmt.Errorf("api.Get(%s): %w", &req, err)
+	}
+	defer resp.Body.Close()
+
+	return nil
 }
